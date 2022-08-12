969a02318f7219295708175e599e4f6c1b849c44
Evans Ochieng <evansochieng121@gmail.com>
2022-08-12 13:15:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+//multiply equation
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+// random equation
+let random = Math.floor(Math.random() * 10) + 1;
+
+// mod equation
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+// max equation
+let max = Math.max(1, 2, 20, 9, 12);
